43bc5f20aa58fa48cbbf02755bb25a09c5401bbb
Pradipas Uttasa <puttasa@z3t9c1.42bangkok.com>
2022-10-06 20:50:02 +0700

    edit

---
+++ ft_strnstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: puttasa <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 19:37:10 by puttasa           #+#    #+#             */
+/*   Updated: 2022/10/06 19:46:38 by puttasa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!*little)
+		return ((char *`)big);
+	while (i < len)
+	{
+		j = i;
+		while (big[i] == little[j] && j < len)
+			j++;
+	}
+}
