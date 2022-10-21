22bcbb11029ee416366db8a3f4eee50959db9446
Pradipas Uttasa <puttasa@z2t6c3.42bangkok.com>
2022-10-21 15:35:43 +0700

    edit

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -5,20 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: puttasa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/20 16:35:45 by puttasa           #+#    #+#             */
-/*   Updated: 2022/10/20 16:39:38 by puttasa          ###   ########.fr       */
+/*   Created: 2022/10/21 15:28:54 by puttasa           #+#    #+#             */
+/*   Updated: 2022/10/21 15:35:10 by puttasa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-voidft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
 		ft_putstr_fd("-2147483648", fd);
 	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
+		n *= -1;
 	}
 	if (n / 10)
 		ft_putnbr_fd(n / 10, fd);
