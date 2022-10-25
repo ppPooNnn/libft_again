caf4741b37ccfe2d9357e175eef2769f787e139a
Pradipas Uttasa <puttasa@z1t3c3.42bangkok.com>
2022-10-25 15:27:43 +0700

    edit

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,19 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: puttasa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/21 18:15:48 by puttasa           #+#    #+#             */
-/*   Updated: 2022/10/21 18:20:18 by puttasa          ###   ########.fr       */
+/*   Created: 2022/10/25 12:53:18 by puttasa           #+#    #+#             */
+/*   Updated: 2022/10/25 15:26:51 by puttasa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_check(char const *set, char const c)
+int	ft_check(char const c, char const *set)
 {
+	int	count;
+
+	count = 0;
 	while (*set)
 	{
 		if (*set == c)
-			return (1);
+			return(1);
 		set++;
 	}
 	return (0);
@@ -25,20 +28,20 @@ int	ft_check(char const *set, char const c)
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	int	head;
+	int	tail;
+	int	len;
+	int	i;
 	char	*new;
-	int		head;
-	int		tail;
-	int		len;
-	int		i;
 
-	i = 0;
-	head = 0;
 	if (!s1 || !set)
 		return (0);
+	i = 0;
+	head = 0;
 	tail = ft_strlen(s1);
-	while (ft_check(set, s1[head]))
+	while (s1[head] && ft_check(s1[head], set))
 		head++;
-	while (ft_check(set, s1[tail - 1]) && tail > head)
+	while (s1[tail - 1] && ft_check(s1[tail - 1], set) && tail > head)
 		tail--;
 	len = tail - head;
 	new = malloc(len + 1);
